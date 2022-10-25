2930273fa83bdd78b4f44489d6551f4052cb78dc
Brian Rogador <brianjos@ualberta.ca>
2022-10-24 21:44:20 -0600

    Finish day 2

--- program.py
+++ program.py
@@ -1,1 +1,10 @@
-# Start writing here
+# Tip Calculator: https://replit.com/@appbrewery/tip-calculator-end
+
+def main():
+    total = float(input("Welcome to the tip calculator.\nWhat was the total bill? $"))
+    tip = 1.0 + (float(input("What percentage tip would you like to give? 10, 12, or 15? ")) / 100)
+    split = int(input("How many people to split the bill? "))
+    print(f"Each person should play: ${round((total * tip / split), 2)}")
+
+if __name__ == "__main__":
+    main()

fcbf326cb2552ea7e1cb1ad9cf9fa57a979967f3
Brian Rogador <brianjos@ualberta.ca>
2022-10-24 18:30:00 -0600

    Finish day 1 program

--- program.py
+++ program.py
@@ -1,1 +1,10 @@
-# Start writing here
+# Band Name Generator: https://replit.com/@appbrewery/band-name-generator-end
+
+def main():
+    city = input("Welcome to the Band Name Generator.\nWhat's the name of the city you grew up in?\n")
+    pet = input("What's your pet's name?\n")
+    print(f"Your band name could be {city} {pet}")
+
+if __name__ == "__main__":
+    main()
+    

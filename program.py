ff0fe9bb2112955ba64811d13279870aaf3ef9fa
Brian Rogador <brianjos@ualberta.ca>
2022-11-05 00:15:28 -0600

    Finish

--- program.py
+++ program.py
@@ -1,1 +1,40 @@
-# Start writing here
+# https://replit.com/@appbrewery/password-generator-end
+def main():
+    #Password Generator Project
+    import random
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    print("Welcome to the PyPassword Generator!")
+    nr_letters= int(input("How many letters would you like in your password?\n")) 
+    nr_symbols = int(input(f"How many symbols would you like?\n"))
+    nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+    min = 0
+    password = ""
+
+    #Eazy Level - Order not randomised:
+    #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+    for nr in range(nr_letters):
+        password += letters[random.randint(min, len(letters) - 1)]
+    
+    for nr in range(nr_symbols):
+        password += symbols[random.randint(min, len(symbols) - 1)]
+
+    for nr in range(nr_numbers):
+        password += numbers[random.randint(min, len(numbers) - 1)]
+
+    #Hard Level - Order of characters randomised:
+    #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+    random_order = ""
+    for char in range(len(password)):
+        random_index = random.randint(0, len(password) - 1)
+        random_char = password[random_index]
+        password = password[:random_index] + password[random_index + 1:]
+        random_order += random_char
+
+    print(f"Your password is: {random_order}")
+
+if __name__ == "__main__":
+    main()
